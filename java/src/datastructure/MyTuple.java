d8678b6b4402c81827b6da5ba15999ccaf895f63
Alto Legato <victor1301166040@gmail.com>
2021-01-05 01:23:01 -0800

    d

---
+++ java/src/datastructure/MyTuple.java
@@ -0,0 +1,16 @@
+package datastructure;
+
+/**
+ * <p>
+ *     A support class for the segment tree class.
+ * </p>
+ */
+public class MyTuple
+{
+    final int a, b;
+    public MyTuple(int a, int b)
+    {
+        this.a = a; this.b = b;
+    }
+
+}
--- java/src/datastructure/SegmentTree.java
+++ java/src/datastructure/SegmentTree.java
@@ -1,5 +1,48 @@
 package datastructure;
 
+import java.util.function.Function;
+
+/**
+ * <p>
+ *     A segment tree over an given array of integers, supports different type of objective function
+ * </p>
+ *
+ *
+ */
 public class SegmentTree
 {
+    public static void main(String[] args)
+    {
+
+    }
+
+    final int arr[];
+    Function<MyTuple, Integer> f;
+
+    public SegmentTree(int[] argin)
+    {
+        arr = argin;
+    }
+
+    public int queryInterval(int a, int b)
+    {
+        throw new RuntimeException("Not Yet Implemented");
+    }
+
+    public void changeElementAt(int a)
+    {
+        if (a < 0 || a >= arr.length) throw new ArrayIndexOutOfBoundsException();
+    }
+
+    protected int getChild(int a)
+    {
+        throw new RuntimeException("Not Yet Implemented");
+    }
+    protected int getParent(int a)
+    {
+        throw new RuntimeException("Not Yet Implemented");
+    }
+
 }
+
+
